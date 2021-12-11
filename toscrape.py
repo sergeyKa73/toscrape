73b61fb62a8490ed86ee22efb9b907fa7d885ad8
Sergey <karandeev73@gmail.com>
2021-12-11 21:20:07 +0300

    feat: add search next page

--- toscrape.py
+++ toscrape.py
@@ -1,19 +1,15 @@
 import requests
 from bs4 import BeautifulSoup as bs
 
-get_html = requests.get('http://books.toscrape.com/')
-if get_html.status_code == 200:
-    html = get_html.content
-
-    soup =bs(html, 'html.parser')
-    sections = soup.select('section')
-    section = sections[0]
-    books_block = section.select_one('ol[class=row]')
-    books = books_block.select('li')
+#сбор данных со страницы
+def get_books(content):
+    soup =bs(content, 'html.parser')
+    block = soup.find('ol', attrs={'class': 'row'})
+    books = block.select('li')
 
     books_data = []
     for book in books:
-        image = 'http://books.toscrape.com/' + book.find('div', attrs={'class': 'image_container'}).find('img')['src']
+        image = book.find('div', attrs={'class': 'image_container'}).find('img')['src']
         title = book.find('h3').find('a')['title']
         price = book.find('p', attrs={'class': 'price_color'}).text
 
@@ -25,4 +21,36 @@ if get_html.status_code == 200:
 
         books_data.append(book_dict)
 
-    print(books_data[3])
+    return books_data
+
+
+#поиск ссылки на  следующую страницу
+def get_next_page(content):
+    soup = bs(content, 'html.parser')
+    try:
+        next_page = 'http://books.toscrape.com/' + soup.find('li', attrs={'class': 'next'}).find('a')['href']
+        return next_page
+    except:
+        pass
+
+final_data = []
+page_number = 1
+url = 'http://books.toscrape.com/catalogue/page-1.html'
+get_html = requests.get(url)
+if get_html.status_code == 200:
+
+    books = get_books(get_html.content)
+    print(f'получено{len(books)} с {page_number} страницы')
+    final_data += books
+
+    next_page = get_next_page(get_html.content)
+    if next_page:
+        page_number += 1
+        get_html = requests.get(next_page)
+        if get_html.status_code == 200:
+            print(f'начинаеи парсить {page_number}')
+        else:
+            pass
+print(f'Данные спарсились {page_number} страниц , {len(final_data)} книг')
+
+
