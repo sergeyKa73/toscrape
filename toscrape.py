4820ab116838b98c3b44d82f5e3d160fd8b845d0
Sergey <karandeev73@gmail.com>
2022-01-01 18:36:19 +0300

    feat: parsing from all pages

--- toscrape.py
+++ toscrape.py
@@ -1,56 +1,73 @@
 import requests
 from bs4 import BeautifulSoup as bs
 
+HOST = 'http://books.toscrape.com/'
+URL = 'http://books.toscrape.com/catalogue/page-1.html'
+HEADERS = {
+    'accept': 'image/avif,image/webp,*/*',
+    'user-agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:95.0) Gecko/20100101 Firefox/95.0'
+}
+
+
+def get_html(url, params=''):
+    r = requests.get(url, headers=HEADERS, params=params)
+    return r
+
 #сбор данных со страницы
-def get_books(content):
-    soup =bs(content, 'html.parser')
+def get_books(html):
+    soup = bs(html, 'html.parser')
     block = soup.find('ol', attrs={'class': 'row'})
     books = block.select('li')
 
     books_data = []
     for book in books:
-        image = book.find('div', attrs={'class': 'image_container'}).find('img')['src']
-        title = book.find('h3').find('a')['title']
-        price = book.find('p', attrs={'class': 'price_color'}).text
+        books_data.append(
+            {
+                'image': book.find('div', attrs={'class': 'image_container'}).find('img')['src'],
+                'title': book.find('h3').find('a')['title'],
+                'price': book.find('p', attrs={'class': 'price_color'}).text
+            }
+        )
 
-        book_dict = {
-            'image': image,
-            'title': title,
-            'price': price
-        }
-        books_data.append(book_dict)
     return books_data
 
 
-#поиск ссылки на  следующую страницу
+
 def get_next_page(content):
     soup = bs(content, 'html.parser')
-    try:
-        next_page = 'http://books.toscrape.com/' + soup.find('li', attrs={'class': 'next'}).find('a')['href']
-        return next_page
-    except:
-        pass
+    block = soup.find('ul', attrs={'class': 'pager'}).find('li', attrs={'class': 'current'}).text
+    len_page = []
+    for i in block:
+        if i.isdigit():
+            len_page.append(int(i))
+    return max(len_page)*10
+
+    # поиск ссылки на  следующую страницу
+    # try:
+    #     next_page = 'http://books.toscrape.com/' + soup.find('li', attrs={'class': 'next'}).find('a')['href']
+    #     print(next_page)
+    #     return next_page
+    # except:
+    #     pass
+
+html = get_html(URL).content
+
 
 final_data = []
 page_number = 1
 url = 'http://books.toscrape.com/catalogue/page-1.html'
 get_html = requests.get(url)
+PAGENATION = get_next_page(html)
 
 if get_html.status_code == 200:
-    while True:
+    for  page in range(1, PAGENATION):
         books = get_books(get_html.content)
-        print(f'получено{len(books)} с {page_number} страницы')
+        print(f'получено{len(books)} с {page} страницы')
         final_data += books
+else:
+    pass
 
-        next_page = get_next_page(get_html.content)
-        if next_page:
-            page_number += 1
-            get_html = requests.get(next_page)
-            if get_html.status_code == 200:
-                print(f'начинаеи парсить {page_number}')
-            else:
-                pass
 
-print(f'Данные спарсились {page_number} страниц , {len(final_data)} книг')
 
+print(f'Данные спарсились {PAGENATION} страниц , {len(final_data)} книг')
 
