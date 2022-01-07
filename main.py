60607fd6559a11fd4ab660e60d85fa389ea01b7b
Sergey <karandeev73@gmail.com>
2022-01-07 17:43:47 +0300

    feat: file main.py add download in xlsx

--- main.py
+++ main.py
@@ -1,9 +1,43 @@
 import json
 import requests
+import xlsxwriter
 from bs4 import BeautifulSoup
 
 PAGES_COUNT = 2
-out_filename = 'out.json'
+OUT_FILENAME = 'out.json'
+OUT_XLSX_FILENAME = 'out.xlsx'
+
+
+def dump_to_json(filename, data, **kwargs):
+    kwargs.setdefault('ensure_ascii', False)
+    kwargs.setdefault('indent', 1)
+
+    with open(OUT_FILENAME, 'w') as f:
+        json.dump(data, f, **kwargs)
+
+
+def dump_to_xlsx(filename, data):
+    if not len(data):
+        return None
+
+    with xlsxwriter.Workbook(filename) as workbook:
+        ws = workbook.add_worksheet()
+        bold = workbook.add_format({'bold': True})
+
+        headers = ['Название товара', 'Цена', 'Ссылка']
+        headers.extend(data[0]['techs'].keys())
+
+        for col, h in enumerate(headers):
+            ws.write_string(0, col, h, cell_format=bold)
+
+        for row, item in enumerate(data, start=1):
+            ws.write_string(row, 0, item['name'])
+            ws.write_string(row, 1, item['amount'])
+            ws.write_string(row, 2, item['url'])
+            for prop_name, prop_value in item['techs'].items():
+                col = headers.index(prop_name)
+                ws.write_string(row, col, prop_value)
+
 
 def get_soup(url, **kwargs):
     response = requests.get(url, **kwargs)
@@ -53,6 +87,7 @@ def parse_products(urls):
             'name': name,
             'amount': amount,
             'techs': techs,
+            'url': url,
         }
         data.append(item)
     return data
@@ -62,9 +97,10 @@ def main():
     urls = crawl_products(PAGES_COUNT)
     print('\n'.join(urls))
     data = parse_products(urls)
+    dump_to_json(OUT_FILENAME, data)
+    dump_to_xlsx(OUT_XLSX_FILENAME, data)
 
-    with open(out_filename, 'w') as f:
-        json.dump(data, f, ensure_ascii=False, indent=1)
+
 
 
 if __name__ == '__main__':
