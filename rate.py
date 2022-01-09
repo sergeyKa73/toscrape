516e897611d5770a178c1ff89a68a56f67ae7a0d
Sergey <karandeev73@gmail.com>
2022-01-09 17:53:41 +0300

    feat: file rate.py add download in csv

--- rate.py
+++ rate.py
@@ -1,17 +1,58 @@
 import requests
 from bs4 import BeautifulSoup as bs
+import fake_useragent
+import csv
 
 URL = 'https://myfin.by/bank/kursy_valjut_nbrb'
-HEADERS = {
-    'accept': 'image/avif,image/webp,image/apng,image/svg+xml,image/*,*/*;q=0.8',
-    'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36'
-}
+user = fake_useragent.UserAgent().random
+header = {'user-agent': user}
+CSV = 'exchange.csv'
 
-source = requests.get(URL, headers=HEADERS)
-main_text  = source.text
-soup = bs(main_text)
 
-table = soup.find('table', attrs={'class': 'default-table'}).getText(separator=';')
+def get_html(url):
+    r = requests.get(url, headers=header)
+    if r.ok:  # 200  ## 403 404
+        return r.text
+    print(r.status_code)
 
-print(table)
 
+def write_csv(data, path):
+    with open(path, 'a') as file:
+        header = ['name', 'cod', 'rate', 'rate_next', 'units', 'link']
+        writer = csv.DictWriter(file, fieldnames=header)
+
+        writer.writeheader()
+
+        writer.writerow(data)
+
+
+def get_page_content(html):
+    soup = bs(html, 'lxml')
+    table = soup.find('tbody').find_all('tr', class_='row body odd')
+    for td in table:
+        line = td.getText(separator=';')
+        name = line.split(';')[0]
+        link = 'https://myfin.by/' + td.find('a')['href']
+        rate = line.split(';')[1]
+        rate_next = line.split(';')[2]
+        cod = line.split(';')[-2]
+        units = line.split(';')[-1]
+
+        data = {
+            'name': name,
+            'link': link,
+            'rate': rate,
+            'rate_next': rate_next,
+            'cod': cod,
+            'units': units
+        }
+
+        write_csv(data, CSV)
+
+
+def main():
+    get_page_content(get_html(URL))
+
+
+if __name__ == '__main__':
+    main()
